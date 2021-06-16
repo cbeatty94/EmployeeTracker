bfb77f921ad98e2eb7257f30d31a84ea21cb2c86
Burke <beatty.burke@gmail.com>
2021-06-16 12:42:20 -0400

    added cases for intitial inquirer prompt and named functions

--- server.js
+++ server.js
@@ -32,4 +32,31 @@ const runTracker = () => {
                 'Update employee roles'
             ]
         })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all employees':
+                    employeeSearch();
+                    break;
+
+                case 'View all employees by department':
+                    departmentSearch();
+                    break;
+
+                case 'View all employees by role':
+                    roleSearch();
+                    break;
+                case 'Add employee':
+                    addEmployee();
+                    break;
+                case 'Add department':
+                    addDepartment();
+                    break;
+                case 'Add role':
+                    addRole();
+                    break;
+                case 'Update employee roles':
+                    updateRole();
+                    break;
+            }
+        })
 }

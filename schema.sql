e3af7432a999c0e92c51b24f9ac63c24fcc0dfac
Burke <beatty.burke@gmail.com>
2021-06-17 10:38:37 -0400

    added foreign key restraints onto tables

--- schema.sql
+++ schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
@@ -16,6 +17,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
@@ -24,3 +26,5 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO employee
